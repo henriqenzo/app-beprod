a68fe6443a1c2a3f0072a732efae165a3b0f2b35
Enzo Henrique <133697357+henriqenzo@users.noreply.github.com>
2025-05-13 16:49:25 -0300

    Initial Commit

---
+++ BeProd/BeProdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeProdApp.swift
+//  BeProd
+//
+//  Created by Enzo Henrique Botelho Romão on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BeProdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeProdTests/BeProdTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeProdTests.swift
+//  BeProdTests
+//
+//  Created by Enzo Henrique Botelho Romão on 13/05/25.
+//
+
+import Testing
+@testable import BeProd
+
+struct BeProdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
